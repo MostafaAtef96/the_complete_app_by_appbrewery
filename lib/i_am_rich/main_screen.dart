88649c40dee4d99aaef7bf8cf71adaf03164416d
MostafaAtef96 <m.atef96@hotmail.com>
2020-08-18 16:41:21 +0200

    :sparkles: I Am Rich App is now complete!

--- lib/i_am_rich/main_screen.dart
+++ lib/i_am_rich/main_screen.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 class IAmRichMainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Text('I Am Rich'),
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+    );
   }
 }
