9a89f3fa6839b4390832146431ab3cf45d3d600e
MostafaAtef96 <m.atef96@hotmail.com>
2020-08-23 18:25:08 +0200

    Custom fonts and UI updates! 	:wrench: Added custom fonts from google fonts. 	:lipstick: More improvements of the UI of MiCard app.

--- lib/mi_card/main_screen.dart
+++ lib/mi_card/main_screen.dart
@@ -12,6 +12,25 @@ class MiCard extends StatelessWidget {
               radius: 50.0,
               backgroundImage: AssetImage('images/MostafaAtef.jpg'),
             ),
+            Text(
+              'Mostafa Ibrahim',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontFamily: 'Pacifico',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'SOFTWARE ENGINEER',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                color: Colors.teal[100],
+                fontFamily: 'SourceSansPro',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ],
         ),
       ),
