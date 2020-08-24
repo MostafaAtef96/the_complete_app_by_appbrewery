c3bab29e04350b7d7b5dbe4e2667513f97c978fe
MostafaAtef96 <m.atef96@hotmail.com>
2020-08-24 03:04:45 +0200

    :sparkles: MiCard App is now complete!

--- lib/mi_card/main_screen.dart
+++ lib/mi_card/main_screen.dart
@@ -5,8 +5,13 @@ class MiCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal,
+      appBar: AppBar(
+        backgroundColor: Colors.teal[900],
+        title: Text('MiCard'),
+      ),
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
               radius: 50.0,
@@ -21,6 +26,13 @@ class MiCard extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            SizedBox(
+              width: 150.0,
+              height: 20.0,
+              child: Divider(
+                color: Colors.teal[900],
+              ),
+            ),
             Text(
               'SOFTWARE ENGINEER',
               style: TextStyle(
@@ -29,6 +41,44 @@ class MiCard extends StatelessWidget {
                 color: Colors.teal[100],
                 fontFamily: 'SourceSansPro',
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 25.0,
+                  color: Colors.teal[900],
+                ),
+                title: Text(
+                  '+20 109 327 7721',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.teal[900],
+                    fontFamily: 'SourceSansPro',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  size: 25.0,
+                  color: Colors.teal[900],
+                ),
+                title: Text(
+                  'm.atef96@hotmail.com',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.teal[900],
+                    fontFamily: 'SourceSansPro',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
           ],
