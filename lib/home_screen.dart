59bef43333ae5bafd0eb78afbf1cc9b09ed51779
MostafaAtef96 <m.atef96@hotmail.com>
2020-08-27 02:33:32 +0200

    :recycle: Alert box refactored

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,6 +6,49 @@ import 'package:the_complete_app_by_appbrewery/I_am_poor/main_screen.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
 class HomeScreen extends StatelessWidget {
+  void showAlertBox({
+    BuildContext context,
+    String alertTitle,
+    String alertDescription,
+    String alertImagePath,
+    String appTitle,
+    dynamic screen,
+  }) {
+    Alert(
+      context: context,
+      title: alertTitle,
+      desc: alertDescription,
+      image: Image.asset(alertImagePath),
+      buttons: [
+        DialogButton(
+          child: Text(
+            "Visit $appTitle",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+            //Go to I Am Rich App.
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => screen,
+              ),
+            );
+          },
+          width: 120,
+        ),
+        DialogButton(
+          child: Text(
+            "Cancel",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () => Navigator.pop(context),
+          width: 120,
+        ),
+      ],
+    ).show();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,47 +66,22 @@ class HomeScreen extends StatelessWidget {
               children: [
                 //This is going to be the application card containing an Image, and the name of the app
                 AppCard(
-                  onPress: () {
-                    Alert(
-                      context: context,
-                      title: 'I Am Rich',
-                      desc:
-                          "I Am Rich App is a very simple application but full of new basic concepts anyone needs to start programming with Flutter.",
-                      image: Image.asset("images/diamond.png"),
-                      buttons: [
-                        DialogButton(
-                          child: Text(
-                            "Visit I Am Rich",
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                            //Go to I Am Rich App.
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => IAmRichMainScreen(),
-                              ),
-                            );
-                          },
-                          width: 120,
-                        ),
-                        DialogButton(
-                          child: Text(
-                            "Cancel",
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
-                          onPressed: () => Navigator.pop(context),
-                          width: 120,
-                        ),
-                      ],
-                    ).show();
-                  },
                   appIconPath: 'images/diamond.png',
                   moduleNumber: 3,
                   appTitle: 'I Am Rich',
                   cardBackgroundColor: Colors.blueGrey[900],
                   appTitleColor: Colors.white,
+                  onPress: () {
+                    showAlertBox(
+                      context: context,
+                      alertTitle: 'I Am Rich',
+                      alertDescription:
+                          'I Am Rich App is a very simple application but full of new basic concepts anyone needs to start programming with Flutter.',
+                      alertImagePath: 'images/diamond.png',
+                      appTitle: 'I Am Rich',
+                      screen: IAmRichMainScreen(),
+                    );
+                  },
                 ),
                 AppCard(
                   onPress: () {
