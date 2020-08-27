705cf97067da34b8a640dab632972e872c52f746
MostafaAtef96 <m.atef96@hotmail.com>
2020-08-27 14:18:27 +0200

    :lipstick: Improved the sizes of application cards in the home screen.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -22,64 +22,70 @@ class HomeScreen extends StatelessWidget {
             child: Column(
               children: [
                 //This is going to be the application card containing an Image, and the name of the app
-                AppCard(
-                  appIconPath: 'images/diamond.png',
-                  moduleNumber: 3,
-                  appTitle: 'I Am Rich',
-                  cardBackgroundColor: Colors.blueGrey[900],
-                  appTitleColor: Colors.white,
-                  onPress: () {
-                    //Open I Am Rich Alert.
-                    showAlertBox(
-                      context: context,
-                      alertTitle: 'I Am Rich',
-                      alertDescription:
-                          'I Am Rich App is a very simple application but full of new basic concepts anyone needs to start programming with Flutter.',
-                      alertImagePath: 'images/diamond.png',
-                      appTitle: 'I Am Rich',
-                      screen: IAmRichMainScreen(),
-                    );
-                  },
+                Expanded(
+                  child: AppCard(
+                    appIconPath: 'images/diamond.png',
+                    moduleNumber: 3,
+                    appTitle: 'I Am Rich',
+                    cardBackgroundColor: Colors.blueGrey[900],
+                    appTitleColor: Colors.white,
+                    onPress: () {
+                      //Open I Am Rich Alert.
+                      showAlertBox(
+                        context: context,
+                        alertTitle: 'I Am Rich',
+                        alertDescription:
+                            'I Am Rich App is a very simple application but full of new basic concepts anyone needs to start programming with Flutter.',
+                        alertImagePath: 'images/diamond.png',
+                        appTitle: 'I Am Rich',
+                        screen: IAmRichMainScreen(),
+                      );
+                    },
+                  ),
                 ),
-                AppCard(
-                  //Image was downloaded from pngwing.com
-                  appIconPath: 'images/poor.png',
-                  moduleNumber: 5,
-                  appTitle: 'I Am Poor',
-                  cardBackgroundColor: Colors.teal[900],
-                  appTitleColor: Colors.white,
-                  onPress: () {
-                    //Open I Am Poor Alert.
-                    showAlertBox(
-                      context: context,
-                      alertTitle: 'I Am Poor',
-                      alertDescription:
-                          'I Am Poor App is similar to the I Am Rich App. The main idea is to practice creating a new Flutter App from Scratch, and following a given Widget Tree to create the desired UI.',
-                      alertImagePath: 'images/poor.png',
-                      appTitle: 'I Am Poor',
-                      screen: IAmPoorMainScreen(),
-                    );
-                  },
+                Expanded(
+                  child: AppCard(
+                    //Image was downloaded from pngwing.com
+                    appIconPath: 'images/poor.png',
+                    moduleNumber: 5,
+                    appTitle: 'I Am Poor',
+                    cardBackgroundColor: Colors.teal[900],
+                    appTitleColor: Colors.white,
+                    onPress: () {
+                      //Open I Am Poor Alert.
+                      showAlertBox(
+                        context: context,
+                        alertTitle: 'I Am Poor',
+                        alertDescription:
+                            'I Am Poor App is similar to the I Am Rich App. The main idea is to practice creating a new Flutter App from Scratch, and following a given Widget Tree to create the desired UI.',
+                        alertImagePath: 'images/poor.png',
+                        appTitle: 'I Am Poor',
+                        screen: IAmPoorMainScreen(),
+                      );
+                    },
+                  ),
                 ),
-                AppCard(
-                  onPress: () {
-                    //Open MiCard Alert.
-                    showAlertBox(
-                      context: context,
-                      alertTitle: 'MiCard',
-                      alertDescription:
-                          'Mi Card is a personal business card. Imagine every time you wanted to give someone your contact details or your business card but you didn\'t have it on you. Well, now you can get them to download your business card as an app.',
-                      alertImagePath: 'images/BusinessCard.png',
-                      appTitle: 'MiCard',
-                      screen: MiCardMainScreen(),
-                    );
-                  },
-                  appIconPath: 'images/BusinessCard.png',
-                  moduleNumber: 6,
-                  appTitle: 'MiCard',
-                  cardBackgroundColor: Colors.teal[900],
-                  appTitleColor: Colors.white,
-                )
+                Expanded(
+                  child: AppCard(
+                    onPress: () {
+                      //Open MiCard Alert.
+                      showAlertBox(
+                        context: context,
+                        alertTitle: 'MiCard',
+                        alertDescription:
+                            'Mi Card is a personal business card. Imagine every time you wanted to give someone your contact details or your business card but you didn\'t have it on you. Well, now you can get them to download your business card as an app.',
+                        alertImagePath: 'images/BusinessCard.png',
+                        appTitle: 'MiCard',
+                        screen: MiCardMainScreen(),
+                      );
+                    },
+                    appIconPath: 'images/BusinessCard.png',
+                    moduleNumber: 6,
+                    appTitle: 'MiCard',
+                    cardBackgroundColor: Colors.teal[900],
+                    appTitleColor: Colors.white,
+                  ),
+                ),
               ],
             ),
           ),
