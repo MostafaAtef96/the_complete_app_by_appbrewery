06169a51ec647b98c43ee9a0075c01a6202d9d89
MostafaAtef96 <m.atef96@hotmail.com>
2020-08-27 14:59:14 +0200

    :lipstick: Dicee UI is created.

--- lib/dicee/main_screen.dart
+++ lib/dicee/main_screen.dart
@@ -3,6 +3,36 @@ import 'package:flutter/material.dart';
 class DiceeMainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Dicee'),
+        backgroundColor: Colors.red,
+      ),
+      body: DicePage(),
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dicee/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dicee/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
