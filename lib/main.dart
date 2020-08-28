4a560d4e2e61c58694127089ddf096b880399d4e
MostafaAtef96 <m.atef96@hotmail.com>
2020-08-28 17:12:31 +0200

    :art: Named routes added to the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:the_complete_app_by_appbrewery/dicee/main_screen.dart';
+import 'package:the_complete_app_by_appbrewery/mi_card/main_screen.dart';
+import 'widgets/app_card.dart';
+import 'i_am_rich/main_screen.dart';
+import 'package:the_complete_app_by_appbrewery/I_am_poor/main_screen.dart';
+import 'package:the_complete_app_by_appbrewery/widgets/visit_app_alert_box.dart';
+import 'package:the_complete_app_by_appbrewery/widgets/exit_app_alert_box.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,6 +21,10 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => HomeScreen(),
+        '/IAmRich': (context) => IAmRichMainScreen(),
+        '/IAmPoor': (context) => IAmPoorMainScreen(),
+        '/MiCard': (context) => MiCardMainScreen(),
+        '/dicee': (context) => DiceeMainScreen(),
       },
     );
   }
