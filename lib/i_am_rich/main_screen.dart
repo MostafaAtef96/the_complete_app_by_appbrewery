3a228faea52fb3e254f0d05d9f97225e1db150a6
MostafaAtef96 <m.atef96@hotmail.com>
2020-09-15 20:36:47 +0200

    :recycle: Refactored I Am Rich app.

--- lib/i_am_rich/main_screen.dart
+++ lib/i_am_rich/main_screen.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:the_complete_app_by_appbrewery/widgets/exit_app_alert_box.dart';
+import 'package:the_complete_app_by_appbrewery/widgets/app_main_widget.dart';
 
 class IAmRichMainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        ExitAppAlertBox(
-          context: context,
-          appTitle: 'I Am Rich',
-          terminate: false,
-        );
-        return false;
-      },
-      child: Scaffold(
+    return AppMainWidget(
+      appTitle: 'I Am Rich',
+      appWidgetTree: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           backgroundColor: Colors.blueGrey[900],
