887747e487b64a7412b5a2ffbc2f23991b3f94d7
MostafaAtef96 <m.atef96@hotmail.com>
2020-09-22 13:50:59 +0200

    :recycle: Refactored MiCard app.

--- lib/mi_card/main_screen.dart
+++ lib/mi_card/main_screen.dart
@@ -1,98 +1,98 @@
 import 'package:flutter/material.dart';
-import 'package:the_complete_app_by_appbrewery/widgets/exit_app_alert_box.dart';
+import 'package:the_complete_app_by_appbrewery/widgets/app_main_widget.dart';
 
 class MiCardMainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        ExitAppAlertBox(
-          context: context,
-          appTitle: 'MiCard',
-          terminate: false,
-        );
-        return false;
-      },
-      child: Scaffold(
-        backgroundColor: Colors.teal,
-        appBar: AppBar(
-          backgroundColor: Colors.teal[900],
-          title: Text('MiCard'),
-        ),
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/MostafaAtef.jpg'),
+    return AppMainWidget(
+      appTitle: 'MiCard',
+      appWidgetTree: MiCard(),
+    );
+  }
+}
+
+class MiCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        backgroundColor: Colors.teal[900],
+        title: Text('MiCard'),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/MostafaAtef.jpg'),
+            ),
+            Text(
+              'Mostafa Ibrahim',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontFamily: 'Pacifico',
+                fontWeight: FontWeight.bold,
               ),
-              Text(
-                'Mostafa Ibrahim',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontFamily: 'Pacifico',
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            SizedBox(
+              width: 150.0,
+              height: 20.0,
+              child: Divider(
+                color: Colors.teal[900],
               ),
-              SizedBox(
-                width: 150.0,
-                height: 20.0,
-                child: Divider(
+            ),
+            Text(
+              'SOFTWARE ENGINEER',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                color: Colors.teal[100],
+                fontFamily: 'SourceSansPro',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 25.0,
                   color: Colors.teal[900],
                 ),
-              ),
-              Text(
-                'SOFTWARE ENGINEER',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  letterSpacing: 2.5,
-                  color: Colors.teal[100],
-                  fontFamily: 'SourceSansPro',
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    size: 25.0,
+                title: Text(
+                  '+20 109 327 7721',
+                  style: TextStyle(
+                    fontSize: 25.0,
                     color: Colors.teal[900],
-                  ),
-                  title: Text(
-                    '+20 109 327 7721',
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      color: Colors.teal[900],
-                      fontFamily: 'SourceSansPro',
-                      fontWeight: FontWeight.bold,
-                    ),
+                    fontFamily: 'SourceSansPro',
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    size: 25.0,
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  size: 25.0,
+                  color: Colors.teal[900],
+                ),
+                title: Text(
+                  'm.atef96@hotmail.com',
+                  style: TextStyle(
+                    fontSize: 25.0,
                     color: Colors.teal[900],
-                  ),
-                  title: Text(
-                    'm.atef96@hotmail.com',
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      color: Colors.teal[900],
-                      fontFamily: 'SourceSansPro',
-                      fontWeight: FontWeight.bold,
-                    ),
+                    fontFamily: 'SourceSansPro',
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
