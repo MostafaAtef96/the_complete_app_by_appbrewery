eadb4da15e4ad7e84f549e65acb241501a4cac58
MostafaAtef96 <m.atef96@hotmail.com>
2020-11-01 21:13:20 +0200

    Structure improvements and UI updates: :art: Improved the structure of the BMI Calculator. :lipstick: Changed the app theme of BMI Calculator.

--- lib/bmi_calculator/main_screen.dart
+++ lib/bmi_calculator/main_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:the_complete_app_by_appbrewery/widgets/app_main_widget.dart';
-import 'package:the_complete_app_by_appbrewery/destini/story_brain.dart';
+import 'package:the_complete_app_by_appbrewery/bmi_calculator/input_page.dart';
 
 class BMIMainScreen extends StatelessWidget {
   @override
@@ -8,20 +8,8 @@ class BMIMainScreen extends StatelessWidget {
     return AppMainWidget(
       appTitle: 'Destini',
       appWidgetTree: Scaffold(
-        body: BMICalculator(),
+        body: InputPage(),
       ),
     );
-  }
-}
-
-class BMICalculator extends StatefulWidget {
-  @override
-  _BMICalculatorState createState() => _BMICalculatorState();
-}
-
-class _BMICalculatorState extends State<BMICalculator> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
