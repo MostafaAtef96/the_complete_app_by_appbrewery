396b0faae3346100cd58eebea6c27c4ad58a4340
MostafaAtef96 <m.atef96@hotmail.com>
2020-11-03 16:01:01 +0200

    :tada: COMMIT #100 :tada: 	:hammer: Created and tested a class for getting the location data.

--- lib/clima/screens/loading_screen.dart
+++ lib/clima/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:the_complete_app_by_appbrewery/clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,24 +7,21 @@ class LoadingScreen extends StatefulWidget {
 }
 
 void getLocation() async {
-  Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.low);
-  print(position);
+  Location location = Location();
+  await location.getCurrentLocation();
+  print(location.latitude);
+  print(location.longitude);
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
