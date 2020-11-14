7e24c452315f2adaa5bb9cd46e4da958666080ee
MostafaAtef96 <m.atef96@hotmail.com>
2020-11-14 23:47:59 +0200

    :poop: Implemented the Mockup of Todoey app.

--- lib/todoey/main_screen.dart
+++ lib/todoey/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:the_complete_app_by_appbrewery/widgets/app_main_widget.dart';
+import 'package:the_complete_app_by_appbrewery/todoey/tasks_screen.dart';
 
 class TodoeyMainScreen extends StatelessWidget {
   static const String todoeyHomeScreenID = 'Todoey';
@@ -8,7 +9,7 @@ class TodoeyMainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppMainWidget(
       appTitle: 'Todoey',
-      appWidgetTree: Container(),
+      appWidgetTree: TasksScreen(),
     );
   }
 }
---
+++ lib/todoey/tasks_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+                left: 20.0, right: 20.0, bottom: 20.0, top: 60.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    color: Colors.lightBlueAccent,
+                    size: 30.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+//              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+              child: Text('Buy Milk!'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
